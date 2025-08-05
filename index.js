dfd4373841c2ff370f05aa1142b37ed78cb81247
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2025-08-05 01:18:14 -0300

    ajustes del codigo mergeado

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ function laCajaDePandora(numero){
         return "El número debe ser un entero.";
     }   
     //  Si es un número par, debe convertirlo a binario y retornarlo, 
-    if(numero % 2 === 0){
-        return numero.toString(2);
-    }
-    // y si es impar, convertirlo a hexadecimal y retornarlo.
-        return numero.toString(16);
+ if(!Number.isInteger(numero)) {
+    return "El numero debe ser un entero"
+}
+if (numero % 2 == 0) {
+    return toString.numero(2);
+}
+return toString.numero(16);
+
 }
 function victoria() {
     return{
@@ -16,15 +19,6 @@ function victoria() {
         edad: 25,
         nacionalidad: "Argentina"
     }
-}
-=======
-if(!Number.isInteger(numero)) {
-    return "El numero debe ser un entero"
-}
-if (numero % 2 == 0) {
-    return toString.numero(2);
-}
-return toString.numero(16);
 }
 
 //En el mismo archivo index.js debes crear una función con tu nombre y 
