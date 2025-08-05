79d522eeb89d1b449789ce7d918141f578f1c0f7
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2025-08-05 00:38:51 -0300

    cambios en la funcion laCajaDePandora

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
+    //que reciba como parámetro un número entero.
+    if(!Number.isInteger(numero)){
+        return "El número debe ser un entero.";
+    }   
+    //  Si es un número par, debe convertirlo a binario y retornarlo, 
+    if(numero % 2 === 0){
+        return numero.toString(2);
+    }
+    // y si es impar, convertirlo a hexadecimal y retornarlo.
+    if(numero % 2 !== 0){
+        return numero.toString(16);
+    }   
 }
